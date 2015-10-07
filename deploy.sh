ce05558be18fb750e39a5e3a60d3b71ca80867c7
Earle Clubb <eclubb@valcom.com>
2015-10-07 01:33:51 -0400

    Only deploy if site has changed.

--- deploy.sh
+++ deploy.sh
@@ -7,12 +7,20 @@ fi
 
 echo 'Checking out master branch' &&
 git checkout master &&
+
 echo 'Copying build files' &&
 cp -r build/* . &&
-echo 'Committing changes' &&
-git add . &&
-git status &&
-git commit -m'Update public site.' &&
-echo 'Deploying site' &&
-git push &&
+
+if ! `git status | grep -q 'nothing to commit, working directory clean'` ; then
+	echo 'Committing changes' &&
+	git status &&
+	git add . &&
+	git commit -m'Update public site.' &&
+
+	echo 'Deploying site' &&
+	git push
+else
+	echo 'Nothing to deploy!'
+fi
+
 git checkout src
