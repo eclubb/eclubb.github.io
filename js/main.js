cfe9ae64a0171f45a378e5d2d1dfaa1f95c9f61d
Earle Clubb <eclubb@valcom.com>
2015-10-08 10:01:07 -0400

    Update public site.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
+$('#navtabs a').click(function (e) {
+  e.preventDefault();
+
+  var url = $(this).attr("data-url");
+  var href = this.hash;
+  var pane = $(this);
+
+  // ajax load from data-url
+  $(href).load(url, function(result){
+    pane.tab('show');
+  });
+});
+
+// load first tab content
+$('#home').load($('#navtabs .active a').attr("data-url"), function(result){
+  $('#navtabs .active a').tab('show');
+});
 
